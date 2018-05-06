eda948c20c995873c73f8f221afeb7cdcf258955
李進興 Vulcan <vulcan@miniasp.com>
2018-05-06 16:44:12 +0800

    Prism EventToCommand 的模擬案例練習

---
+++ XFEventToCmd/XFEventToCmd/XFEventToCmd/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace XFEventToCmd.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public double SliderValue { get; set; }
+        public double ContainerValue { get; set; }
+        public DelegateCommand ValueChangedCommand { get; set; }
+        private readonly INavigationService _navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            ValueChangedCommand = new DelegateCommand(() =>
+            {
+                double fooHeight = 294;
+                ContainerValue = fooHeight * (SliderValue / 100.0);
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+
+        }
+
+    }
+}
